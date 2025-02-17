65ba680699a936a73f9c6d5c1c60385f237a9042
MihaiAC <alexmihai.ciobanu@gmail.com>
2025-02-17 17:19:50 +0200

    Change new project structure.
    Adopt the two scoops project layout recommendations.

--- learning_django/django_forms/config/settings/settings.py
+++ learning_django/django_forms/config/settings/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "forms_app.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "forms_app.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
